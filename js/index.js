59817ad00e7e023cb8177010665c069109b4fc64
Róisín Grannell <r.grannell2@gmail.com>
2025-05-24 13:56:46 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+const $input = document.querySelector('#idiot-says');
+const $output = document.querySelector('#spongebob-output');
+
+function spongebobify(text) {
+  return text.split('').map((char, index) => {
+    return index % 2 === 0 ? char.toLowerCase() : char.toUpperCase();
+  }).join('');
+}
+
+function onInput() {
+  const value = $input.value;
+
+  if (value.trim() === '') {
+    $output.textContent = '';
+    return;
+  }
+  const spongebobText = spongebobify(value);
+  console.log(spongebobText);
+  $output.innerHTML = spongebobText;
+}
+
+$input.addEventListener('keydown', onInput);
